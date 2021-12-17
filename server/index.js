cb262ee3d556179038ab4d3ecf5c92683efee44b
Lynda <l_bounif@esi.dz>
2021-12-17 12:32:01 +0100

    auth app back front

---
+++ server/index.js
@@ -0,0 +1,14 @@
+//Register APP
+//1. Create the API:
+//1.1 init app: npm, git, gitignore and all structure API
+//1.2 create server express
+//1.3 create mongoose DB connexion
+//1.4 Create user model: firstName, lastName, email, password, 
+//country, state, city
+//1.5 create register route and its function in controllers
+
+//2.Create the components 
+//2.1 create 6 components: Header, FirstStep, SecondStep, ThirdStep, 
+//Progress, Login
+//Header: contain H1 and Progress
+//2.2 Create router
