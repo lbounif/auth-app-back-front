be3e324d2c62f33df28aa787f328bbcaa3b28c81
Lynda <l_bounif@esi.dz>
2021-12-18 12:50:58 +0100

    add useForm hook for input verifications

--- src/components/FirstStep.js
+++ src/components/FirstStep.js
@@ -1,20 +1,81 @@
-
+import { useForm } from 'react-hook-form'
 import { Form, Button } from 'react-bootstrap'
-const FirstStep = ({user}) => {
+
+const FirstStep = (props) => {
+    const {user} = props
+    //for controlling form inputs
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        defaultValues: {
+            firstName: user.firstName,
+            lastName: user.lastName
+        }
+    });
+
+    const onSubmit = async (data) => {
+        // user = {
+        //     firstName: "",
+        //     lastName: ""
+        // }
+        // updateUsser -> user = {
+        //     firstName: "Sara",
+        //     lastName: "Sara"
+        // }
+        //update user with entered values
+        props.updateUser(data)
+        //redirect to secondStep component
+        props.history.push('/second') 
+    }
+
     return (
-        <Form className="input-form">
-            <Form.Group className="mb-3" controlId="firstName">
-                <Form.Label>First Name</Form.Label>
-                <Form.Control type="text" placeholder="Enter FirstName" />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="lastName">
-                <Form.Label>Last Name</Form.Label>
-                <Form.Control type="text" placeholder="lastName" />
-            </Form.Group>
-            <Button variant="primary" type="submit" className="btn">
-                Suivant
-            </Button>
-        </Form>
+    <Form className="input-form" onSubmit={handleSubmit(onSubmit)} >
+        <Form.Group controlId="firstName">
+            <Form.Label className="label"> First Name </Form.Label>
+            <Form.Control 
+                type="firstName"
+                name="firstName"
+                placeholder="Enter your First Name"
+                {...register('firstName', {
+                    required: 'First Name is required',
+                    pattern: {
+                        value: /^[a-zA-Z]/,
+                        message: 'First Name should contain only caracters'
+                    }
+                })}
+                className={`${errors.firstName ? 'input-error': ''}`}
+                />
+                {/* if error exists display it */}
+                {   
+                    errors.firstName && (
+                    <p className="errorMsg">{errors.firstName.message}</p>
+                )}
+        </Form.Group>
+
+        <Form.Group controlId="lastName">
+            <Form.Label className="label"> Last Name </Form.Label>
+            <Form.Control 
+                type="lastName"
+                name="lastName"
+                placeholder="Enter your Last Name"
+                {...register("lastName", {
+                    required: 'Last Name is required',
+                    pattern: {
+                        value: /^[a-zA-Z]/,
+                        message: 'Last Name should contain only caracters'
+                    }
+                })}
+                className={`${errors.lastName ? 'input-error': ''}`}
+                />
+                {/* if error exists display it */}
+                { 
+                errors.lastName && (
+                    <p className="errorMsg">{errors.lastName.message}</p>
+                )}
+        </Form.Group>
+
+        <Button type="submit" variant="primary" className="btn">
+            Next
+        </Button>
+    </Form>
     )
 }
 
--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -8,6 +8,11 @@ import Login from "../components/Login"
 
 const AppRouter = () => {
     const [user, setUser] = useState({})
+
+    const updateUser = (data) => {
+        setUser((prevUser)=> ({...prevUser, ...data}))
+    }
+
     return (
         <BrowserRouter>
             <div className="container">
@@ -17,9 +22,9 @@ const AppRouter = () => {
                         // render={(props)=> (
                         //     <FirstStep {...props} user={user}/>
                         // )}
-                        element={<FirstStep/>}
+                        element={<FirstStep user={user} updateUser={updateUser}/>}
                         path="/"
-                        exact={true}/>
+                        exact/>
                     <Route
                         element={<SecondStep/>}
                         path="/second"
@@ -32,6 +37,7 @@ const AppRouter = () => {
                         element={<Login/>}
                         path="/login"
                         exact={true}/>
+                    {/* <Route render={() => <Redirect to="/" />} /> */}
                 </Routes>
             </div>
         </BrowserRouter>
